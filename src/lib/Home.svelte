4b4a20f605ca70a5bf37dd85132a8545ef665a11
connorjbryant <connorjamesbryant7@gmail.com>
2022-02-20 00:04:29 -0500

    Working on firebase login

--- src/lib/Home.svelte
+++ src/lib/Home.svelte
@@ -1,5 +1,3 @@
-<h1 class="title home">Grim Pickings</h1>
-
 <style>
 	@import '$lib/css/screens.css';
 
@@ -7,3 +5,9 @@
 		margin-top: 325px;
 	}
 </style>
+
+<h1 class="title home">Grim Pickings</h1>
+
+<input type="text" name="name" placeholder="First Name" id="name" required="required" />
+<input type="email" name="username" placeholder="Email" id="email" required="required" />
+<button onclick="signUp()" id="signUp" class="btn btn-primary btn-block btn-large">Sign Up</button>
